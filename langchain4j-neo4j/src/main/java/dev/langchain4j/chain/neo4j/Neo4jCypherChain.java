ad09593aa5a67db480bf5f1a3c2a9354ecc81192
snayak15 <Siben_Nayak@intuit.com>
2024-03-03 15:44:04 +0530

    feat:initial commit

---
+++ langchain4j-neo4j/src/main/java/dev/langchain4j/chain/neo4j/Neo4jCypherChain.java
@@ -0,0 +1,12 @@
+package dev.langchain4j.chain.neo4j;
+
+import dev.langchain4j.chain.Chain;
+
+public class Neo4jCypherChain implements Chain<String, String> {
+
+    @Override
+    public String execute(String query) {
+
+        return null;
+    }
+}
